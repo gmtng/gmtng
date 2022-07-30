bf8ffe55358ffc5dc4b68f24a8f2400f52c75797
gmtng <110279613+gmtng@users.noreply.github.com>
2022-07-30 20:05:18 +0800

    Add files via upload
    新手练习

---
+++ yuan.c
@@ -0,0 +1,13 @@
+#include<stdio.h>//预处理指令 头文件
+int main(viod)/*返回类型以及c的基本模块*/
+{
+	int num;//声明变量
+	num = 1;//赋值（C的基本操作之一）为num预留空间
+
+	printf("I am a simple");//标准函数printf()
+	printf("computer.\n");
+	printf("My favorier  number is %d because it is first.\n", num);
+
+	getchar();
+	return 0;
+}
