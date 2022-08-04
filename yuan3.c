171a3c86b050ba0929e9e3c6ac873a58dc17ccf5
gmtng <110279613+gmtng@users.noreply.github.com>
2022-08-04 17:10:05 +0800

    Update yuan3.c

--- yuan3.c
+++ yuan3.c
@@ -7,17 +7,21 @@ int main(void)
 
 	printf("Are you worth your wewihgt in platinum?\n ");
 	printf("Let's check ti out.\n");
-	printf("please enter your weight in pounds:");
-
-	//获取输入_英磅
-	scanf("%f",&weight);//(´。＿。｀)
-	//设白金&1700/盎司
-	//1英磅=14.5833盎司
+	printf（“请输入您的体重（以磅为单位）：”）；
+		
+	getchar();//等待用户输入，输入后回车继续
+	scanf_s("%f",&weight);
+	//VS编译器特有，觉得scanf()不安全，自己定义了scanf_s(）
+	//获取输入_英磅	
 
 	value = 1700.0 * weight * 14.5833;
+	//1英磅=14.5833盎司,设白金&1700/盎司
+	
 	printf("your weight in platunm $%.2f.\n", value);
 	printf("you are easily woth that!if platinum prices drop,\n");
 	printf("eat more to maintain your value.\n");
 
 	return 0;
-}//一个有bag的程序跑不动。╥﹏╥...
+}
+//一个有bag的程序跑不动。╥﹏╥... 
+//现在跑得动了。(￣y▽,￣)╭ 
