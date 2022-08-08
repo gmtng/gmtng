f097f088e2f89920063a4a9a6ae8fd6bd0f200a4
gmtng <110279613+gmtng@users.noreply.github.com>
2022-08-09 00:00:30 +0800

    Add files via upload

--- yuan8.c
+++ yuan8.c
@@ -1,3 +1,6 @@
+//0x00007FF7FA7F30FD 处有未经处理的异常(在 Project8.exe 中): 堆栈 Cookie 检测代码检测到基于堆栈的缓冲区溢出。
+//以上问题是由于高版本的VS所带来的，可以用下行代码解决。(scanf就不要加_s了）
+#pragma warning(disable:4996)
 #include<stdio.h>
 #include<string.h>
 #define DENSITY 62.4
@@ -5,12 +8,12 @@ int main(void)
 {
 	float weight, volume;
 	int size, letters;
-	char name[40];
+	char name[40];//(这里有问题但我解决不了)重新梳理一下函数的逻辑，使用正确的索引即可
 
 	printf("Hi!what's your first name\n");
-	scanf_s("%s",name);
+	scanf("%s",name);
 	printf("%s,what's your weight in pounds?\n",name);
-	scanf_s("%f",&weight);
+	scanf("%f",&weight);
 	size = sizeof(name);
 	letters = strlen(name);
 	volume = weight / DENSITY;
@@ -20,3 +23,5 @@ int main(void)
 
 	return 0;
 }
+//下次再来解决这个bag吧(┬┬﹏┬┬)
+//bag已解决(￣y▽,￣)╭ 
