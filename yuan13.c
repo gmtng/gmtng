bf29c6a6a60db33e61ea5f0b68e5aa6315c8717b
gmtng <110279613+gmtng@users.noreply.github.com>
2022-08-26 23:15:43 +0800

    Add files via upload

--- yuan13.c
+++ yuan13.c
@@ -6,11 +6,11 @@ int main(void)
 	
 	printf("Convert seconds to minutes and seconds!\n");
 	printf("Enter the numder of seconds (<=0 to quit):\n");
-	scanf_s("%d", &sec);
-	while (sec > 0)
+	scanf_s("%d", &sec);//读取秒数
+	while (sec > 0)//别打分号！
 	{
-		min = sec / SCE_PER_MIN;
-		left = sec % SCE_PER_MIN;
+		min = sec / SCE_PER_MIN;//截断分钟数
+		left = sec % SCE_PER_MIN;//剩下秒数
 		printf("%d seconds is %d minutes,%d ceconds.\n",sec,min,left);
 		printf("Enter next value (<=0 to quit):\n");
 		scanf_s("%d",&sec);
